6dd00a6e588d166a953c5ee4174b3e6590789e17
Ha Lee <halee20@users.noreply.github.com>
2016-11-08 01:22:41 -0600

    1
    1

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,61 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+  def show
+
+    p = Photo.find_by({ :id => params[:id]})
+    @photo_source = p.source
+    @photo_caption = p.caption
+    @photo_id = p.id
+
+
+  end
+
+  def new_form
+
+  end
+
+  def create_row
+    p = Photo.new
+    p.caption = params[:the_caption]
+    p.source = params[:the_source]
+    p.save
+
+    #render("create_row.html.erb")
+    redirect_to("http://localhost:3000/photos")
+  end
+
+  def delete_row
+    p = Photo.find_by({ :id => params[:id]})
+    p.destroy
+
+    redirect_to("http://localhost:3000/photos")
+
+  end
+
+  def edit_form
+
+    p = Photo.find_by({ :id => params[:id]})
+    @photo_source = p.source
+    @photo_caption = p.caption
+    @photo_id = p.id
+
+
+  end
+
+  def update_row
+
+
+    p = Photo.find_by({ :id => params[:id]})
+    p.source = params[:the_source]
+    p.caption = params[:the_caption]
+    p.save
+
+    redirect_to("http://localhost:3000/photos")
+
+
+
+  end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,21 @@
 Rails.application.routes.draw do
 
   get("/", { :controller => "photos", :action => "index" })
+  #Routes to create photos
+  get("/photos/new",  { :controller =>"photos", :action => "new_form"})
+  get("/create_photo",  { :controller =>"photos", :action => "create_row"})
+
+  #Routes to DELETE photos
+  get("/delete_photo/:id",  { :controller =>"photos", :action => "delete_row"})
+
+  #Routes to EDIT photos
+  get("/photos/:id/edit",  { :controller =>"photos", :action => "edit_form"})
+  get("/update_row/:id",  { :controller =>"photos", :action => "update_row"})
 
   # Routes to READ photos
   get("/photos",           { :controller => "photos", :action => "index" })
   get("/photos/:id",       { :controller => "photos", :action => "show" })
+
+
 
 end
